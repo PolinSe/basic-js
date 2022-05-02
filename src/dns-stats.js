51ea80289ac4af210e623bbc71f687fb42d97c20
Polina Sedunova <lina.malina.spb@gmail.com>
2022-05-02 17:42:28 +0300

    feat: start tasks

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {}
+
+  for (let i = 0; i < domains.length; i++){
+    let arr = domains[i].split('.').reverse()
+    console.log(arr)
+  }
+  return result
 }
 
 module.exports = {
